db87dda65a0bcc5ad2d8ad252be3aae2a05c84ab
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-06-29 19:13:14 +0300

    Update VeriAnaliziFinal.py

--- VeriAnaliziFinal.py
+++ VeriAnaliziFinal.py
@@ -101,7 +101,7 @@ def VeriGorsellestirme(degisken1,hedefDegisken,q,tol,line):
 
 
 #streamlit run VeriAnaliziFinal.py --server.maxUploadSize=1028
-st.write("Made by Analytics Team")
+st.sidebar.header("Veri Görselleştirme")
 try:
     df = load_data()  
 except:
